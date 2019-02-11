4be0a08bf8104f0c523090ef38e89c9412725cbc
magda-u <magdalena.urzon@gmail.com>
2019-02-11 17:01:26 +0100

    add javascript

---
+++ README.md
@@ -0,0 +1,17 @@
+# Weekly Challenge 37
+
+Challenge from facebok group Weekly WebDev Challenge  
+PSD to html 
+
+## Technologies
+* html
+* CSS
+* javascript
+
+## Source
+
+fb group Weekly WebDev Challenge: https://www.facebook.com/groups/940002776068923/  
+PSD: https://drive.google.com/open?id=0Bw2hu70L5Ye_RVNaYU5nclNjeUk
+
+## Live
+https://magdalena-u.github.io/challenge-37
--- script.js
+++ script.js
@@ -1,9 +1,97 @@
-const menu = document.querySelector('.main_nav');
+const menu = document.querySelector('.nav_label');
 const navCont = document.querySelector('.nav_content');
+
+/*our creative team */
+
+const leftArr = document.querySelector('.arrow1');
+const rightArr = document.querySelector('.arrow2');
+const divTeam = document.querySelectorAll('.team_imgs>div');
+let n = 1;
+
+// left click - do naprawy
+
+leftClick = () => {
+    document.querySelector('.active_img').classList.remove('active_img');
+    document.querySelector('.first').classList.remove('first');
+    document.querySelector('.third').classList.remove('third');
+    if (n >= 1) {
+        n--;
+        let prev = n - 1;
+        let next = n + 1;
+        if (prev < 0) {
+            prev = 2;
+        }
+        divTeam[n].classList.add('active_img');
+        divTeam[prev].classList.add('first');
+        divTeam[next].classList.add('third');
+        console.log(prev);
+        console.log(n);
+        console.log(next);
+    } else {
+        n = 2;
+        let prev = n - 1;
+        let next = 0;
+        divTeam[n].classList.add('active_img');
+        divTeam[prev].classList.add('first');
+        divTeam[next].classList.add('third');
+        console.log(prev);
+        console.log(n);
+        console.log(next);
+    }
+}
+
+rightClick = () => {
+    document.querySelector('.active_img').classList.remove('active_img');
+    document.querySelector('.first').classList.remove('first');
+    document.querySelector('.third').classList.remove('third');
+    if (n < 2) {
+        n++;
+        let prev = n - 1;
+        let next = n + 1;
+        if (next > 2) {
+            next = 0
+        }
+        divTeam[n].classList.add('active_img');
+        divTeam[prev].classList.add('first');
+        divTeam[next].classList.add('third');
+    } else {
+        n = 0;
+        let prev = 2;
+        let next = n + 1;
+        divTeam[n].classList.add('active_img');
+        divTeam[prev].classList.add('first');
+        divTeam[next].classList.add('third');
+    }
+}
+
+
+leftArr.addEventListener('click', leftClick);
+rightArr.addEventListener('click', rightClick);
+
+//what client says
+
+const clientFirst = document.querySelector('.client_img1');
+const clientSecond = document.querySelector('.client_img2');
+const clientThird = document.querySelector('.client_img3');
+const clientMain = document.querySelector('.client_main_img');
+
+clientFirst.addEventListener('click', function () {
+    clientMain.style.backgroundImage = 'url(images/client1.png)';
+})
+
+clientSecond.addEventListener('click', function () {
+    clientMain.style.backgroundImage = 'url(images/team2.png)';
+})
+
+clientThird.addEventListener('click', function () {
+    clientMain.style.backgroundImage = 'url(images/team3.png)';
+})
+
+
+/* menu - mobile version */
 
 const openMenu = () => {
     navCont.classList.toggle('active');
 }
 
 menu.addEventListener('click', openMenu);
-
